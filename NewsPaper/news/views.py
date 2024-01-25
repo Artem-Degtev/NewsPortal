f801118c20fca553bca285a7a8bf958c63f5cd7f
Artem-Degtev <129773998+Artem-Degtev@users.noreply.github.com>
2024-01-25 12:55:46 +0300

    Homework D13 v.2

--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -14,8 +14,98 @@ from django.views.decorators.csrf import csrf_protect
 from .models import Subscription, Category
 from django.shortcuts import redirect, get_object_or_404
 import logging
+import logging.handlers
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+
+console_handler.setFormatter(formatter)
+
+logger.addHandler(console_handler)
+
+# Создание файлового обработчика для записи логов в файл general.log
+file_handler = logging.FileHandler('general.log')
+file_handler.setLevel(logging.INFO)
+
+# Создание форматтера для задания формата вывода в файл
+file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+
+# Добавление файлового обработчика к основному логгеру
+logger.addHandler(file_handler)
+
+# Создание файлового обработчика для записи логов уровня ERROR и выше в файл errors.log
+errors_file_handler = logging.FileHandler('errors.log')
+errors_file_handler.setLevel(logging.ERROR)
+
+# Создание форматтера для задания формата вывода в файл errors.log
+errors_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s - %(pathname)s\n%(exc_info)s')
+errors_file_handler.setFormatter(errors_formatter)
+
+# Добавление файлового обработчика к определенным регистраторам Django
+django_loggers = ['django.request', 'django.server', 'django.template', 'django.db.backends']
+for logger_name in django_loggers:
+    logger = logging.getLogger(logger_name)
+    logger.addHandler(errors_file_handler)
+
+# Создание файлового обработчика для записи логов из регистратора django.security
+security_file_handler = logging.FileHandler('security.log')
+security_file_handler.setLevel(logging.DEBUG)  # Установите уровень логирования по необходимости
+
+# Создание форматтера для задания формата вывода в файл security.log
+security_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+security_file_handler.setFormatter(security_formatter)
+
+# Добавление файлового обработчика к регистратору django.security
+security_logger = logging.getLogger('django.security')
+security_logger.addHandler(security_file_handler)
+
+
+# Создание почтового обработчика для отправки сообщений уровня ERROR и выше на почту
+mail_handler = logging.handlers.SMTPHandler(
+    mailhost=('smtp.yandex.ru', 465),
+    fromaddr='degtevag@yandex.ru',
+    toaddrs='artemdegtev@gmail.ru',
+    subject='Error from your application',
+    credentials=('degtevag', 'pflmrrodkpbwmqdd'),
+    secure=()
+)
+mail_handler.setLevel(logging.ERROR)
+
+# Создание форматтера для задания формата вывода на почту
+mail_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s - %(pathname)s')
+mail_handler.setFormatter(mail_formatter)
+
+# Добавление почтового обработчика к регистраторам django.request и django.server
+django_request_logger = logging.getLogger('django.request')
+django_request_logger.addHandler(mail_handler)
+
+django_server_logger = logging.getLogger('django.server')
+django_server_logger.addHandler(mail_handler)
+
+# Установка фильтрации для разных обработчиков в зависимости от значения DEBUG
+class DebugFilter(logging.Filter):
+    def filter(self, record):
+        return not getattr(record, 'is_debug', False)
+
+# Создание обработчика для вывода логов в консоль
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+
+# Применение фильтра к обработчику для вывода логов в консоль
+console_handler.addFilter(DebugFilter())
+
+# Добавление обработчика к основному логгеру
+logger.addHandler(console_handler)
+
+# Применение фильтра к обработчикам для файла и почты
+file_handler.addFilter(DebugFilter())
+mail_handler.addFilter(DebugFilter())
 
 def index(request):
     logger.info('INFO')
